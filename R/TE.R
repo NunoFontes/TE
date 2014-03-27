7e7af413c1809c4022a8c0750910826433d66d13
Nuno Fontes <Nuno@tradingeconomics.com>
2014-03-27 09:11:45 +0000

    test

--- R/TE.R
+++ R/TE.R
@@ -921,6 +921,11 @@ te.correlation.matrix=function(c,country,indicator,d1="NULL",opts=NULL){
 #AMERICAS=
 #ASIA= 
 
+te.testObj = function(obj){
+  plot(1:10)
+}
+
+
 indicators=c("gdp annual growth rate","inflation rate")
 countries=c("portugal","greece","spain","united states","united kingdom","japan")
 
@@ -972,36 +977,38 @@ te.simplecorrelation.matrix=function(c,country,indicator,d1="NULL",opts=NULL){
 }
 
 ## Function for arranging ggplots. use png(); arrange(p1, p2, ncol=1); dev.off() to save.
-require(grid)
-vp.layout <- function(x, y) viewport(layout.pos.row=x, layout.pos.col=y)
+#require(grid)
+#vp.layout <- function(x, y) viewport(layout.pos.row=x, layout.pos.col=y)
 
-pl1 = te.plot.multi(1,country,indicator[1])
-pl2 = te.plot.multi(1,country,indicator[2])
-pl3 = te.plot.multi(1,country,indicator[3])
+#pl1 = te.plot.multi(1,country,indicator[1])
+#pl2 = te.plot.multi(1,country,indicator[2])
+#pl3 = te.plot.multi(1,country,indicator[3])
 
-arrange_ggplot2(pl1,pl2,pl3,ncol=1)
+#arrange_ggplot2(pl1,pl2,pl3,ncol=1)
 
-arrange_ggplot2 <- function(..., nrow=NULL, ncol=NULL, as.table=FALSE) {
-  dots <- list(...)
-  n <- length(dots)
-  if(is.null(nrow) & is.null(ncol)) { nrow = floor(n/2) ; ncol = ceiling(n/nrow)}
-  if(is.null(nrow)) { nrow = ceiling(n/ncol)}
-  if(is.null(ncol)) { ncol = ceiling(n/nrow)}
-  ## NOTE see n2mfrow in grDevices for possible alternative
-  grid.newpage()
-  pushViewport(viewport(layout=grid.layout(nrow,ncol) ) )
-  ii.p <- 1
-  for(ii.row in seq(1, nrow)){
-    ii.table.row <- ii.row	
-    if(as.table) {ii.table.row <- nrow - ii.table.row + 1}
-    for(ii.col in seq(1, ncol)){
-      ii.table <- ii.p
-      if(ii.p > n) break
-      print(dots[[ii.table]], vp=vp.layout(ii.table.row, ii.col))
-      ii.p <- ii.p + 1
-    }
-  }
-}
+#arrange_ggplot2 <- function(..., nrow=NULL, ncol=NULL, as.table=FALSE) {
+#  dots <- list(...)
+#  n <- length(dots)
+#  if(is.null(nrow) & is.null(ncol)) { nrow = floor(n/2) ; ncol = ceiling(n/nrow)}
+#  if(is.null(nrow)) { nrow = ceiling(n/ncol)}
+#  if(is.null(ncol)) { ncol = ceiling(n/nrow)}
+#  ## NOTE see n2mfrow in grDevices for possible alternative
+#  grid.newpage()
+#  pushViewport(viewport(layout=grid.layout(nrow,ncol) ) )
+#  ii.p <- 1
+#  for(ii.row in seq(1, nrow)){
+#    ii.table.row <- ii.row	
+#    if(as.table) {ii.table.row <- nrow - ii.table.row + 1}
+#    for(ii.col in seq(1, ncol)){
+#      ii.table <- ii.p
+#      if(ii.p > n) break
+#      print(dots[[ii.table]], vp=vp.layout(ii.table.row, ii.col))
+#      ii.p <- ii.p + 1
+#    }
+#  }
+#}
+
+
 
 #historical.matrix = function(c,countries,indicators,d1="2005"){
 #  options(stringsAsFactors = FALSE)
