617a0b61fa2cef859afffe7f39be550c9c76de17
Nuno Fontes <Nuno@tradingeconomics.com>
2014-03-28 16:43:04 +0000

    a

--- R/TE.R
+++ R/TE.R
@@ -956,10 +956,11 @@ te.historicalPlusForecasts=function(country,indicator,DateTime,Value,Model,LastU
   theHistorical=te.get.hist.multi.free.new(country,indicator,d1)
   theHistorical$DateTime = as.Date(theHistorical$DateTime)
   theforecasts$DateTime = as.Date(theforecasts$DateTime)
-  dataFrame=rbind(theforecasts[c("DateTime","Close","Category")],tail(theHistorical[c("DateTime","Close","Category")],25))
+  dataFrame=rbind(tail(theHistorical[c("DateTime","Close","Category")],25),theforecasts[c("DateTime","Close","Category")])
   ggplot(dataFrame,aes(x=DateTime, y=Close, colour=Category)) + 
     geom_line() + 
     xlab("") + ylab("") +
+    geom_line(data=dataFrame[dataFrame$Category==indicator, ], aes(x=DateTime, y=Close), colour="black", size=1) +
     theme(axis.text.y = element_blank(), 
           panel.border=element_blank(),
           axis.line=element_line(colour = "grey",size=.3),
